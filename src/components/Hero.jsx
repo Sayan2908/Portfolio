ff33d2ee8f0384d9ed84c791e80893fea543259c
Sayan Dey Sarkar <sayandeysarkar2003@gmail.com>
2023-07-18 03:08:52 +0530

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#ff1a1a]'>Sayan</span>
+            Hi,<span className='text-[#ff1a1a]'>Sayan</span> this side
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am a web and app developer<br className='sm:block hidden' />
@@ -24,11 +24,11 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <>
+      
       <ComputersCanvas/>        
-      </>
+      
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center z-50'>
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
--- src/constants/index.js
+++ src/constants/index.js
@@ -46,7 +46,7 @@ const services = [
     icon: web,
   },
   {
-    title: "React Native Developer",
+    title: "App Developer",
     icon: mobile,
   },
   {
@@ -54,7 +54,7 @@ const services = [
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "Game Developer",
     icon: creator,
   },
 ];
@@ -81,7 +81,7 @@ const technologies = [
     icon: reactjs,
   },
   {
-    name: "Redux Toolkit",
+    name: "Next JS",
     icon: redux,
   },
   {
@@ -97,8 +97,8 @@ const technologies = [
     icon: mongodb,
   },
   {
-    name: "Three JS",
-    icon: threejs,
+    name: "Django",
+    icon: meta,
   },
   {
     name: "git",
@@ -116,55 +116,38 @@ const technologies = [
 
 const experiences = [
   {
-    title: "React.js Developer",
-    company_name: "Starbucks",
+    title: "Virtual Reality Developer",
+    company_name: "3s Groomers",
     icon: starbucks,
     iconBg: "#383E56",
     date: "March 2020 - April 2021",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Developed interactive scenes in Unity UI, optimizing UI design and scripting for enhanced user experiences",
+      "Integrated character animations seamlessly in Unity and Flutter projects, enhancing appeal and engagement",
+      "Utilized Flutter framework to develop cross-platform mobile applications integrated with unity",
     ],
   },
   {
-    title: "React Native Developer",
-    company_name: "Tesla",
+    title: "Freelancer",
+    company_name: "Freelancer",
     icon: tesla,
     iconBg: "#E6DEDD",
     date: "Jan 2021 - Feb 2022",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Developing and maintaining web applications using Next.js and other related technologies mainly in react framework",
+      "Developing and maintaining mobile applications using Flutter",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
       "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
   {
-    title: "Web Developer",
+    title: "Waiting",
     company_name: "Shopify",
     icon: shopify,
     iconBg: "#383E56",
     date: "Jan 2022 - Jan 2023",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Full stack Developer",
-    company_name: "Meta",
-    icon: meta,
-    iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Waiting🥰",
     ],
   },
 ];
@@ -198,9 +181,9 @@ const testimonials = [
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "Campus Catalogue",
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      " Developed an e-commerce web application for campus residents, including portals for customers, shopkeepers, and admin management and enabled seamless item management by shopkeepers and efficient ordering by customers",
     tags: [
       {
         name: "react",
@@ -214,51 +197,55 @@ const projects = [
         name: "tailwind",
         color: "pink-text-gradient",
       },
+      {
+        name: "nodejs",
+        color: "green-text-gradient",
+      }
     ],
     image: carrent,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/Sayan2908/campus_catalogue/",
   },
   {
-    name: "Job IT",
+    name: "Client App",
     description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      "Developed a productivity app for industry workers using Flutter and Firebase, leveraging the power of Google authentication for secure user login aimed to replace traditional Excel-based workflow management.",
     tags: [
       {
-        name: "react",
+        name: "flutter",
         color: "blue-text-gradient",
       },
       {
-        name: "restapi",
+        name: "firebase",
         color: "green-text-gradient",
       },
       {
-        name: "scss",
+        name: "firestore",
         color: "pink-text-gradient",
       },
     ],
     image: jobit,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/vrishank97/protofac-app/",
   },
   {
-    name: "Trip Guide",
+    name: "Product Reviewer",
     description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      " Developed a Python Flask web-scraping tool search and retrieve item reviews from top online marketplaces like Flipkart and Amazon.Empowered users to make informed purchasing decisions by providing a better understanding of products.",
     tags: [
       {
-        name: "nextjs",
+        name: "nuxtjs",
         color: "blue-text-gradient",
       },
       {
-        name: "supabase",
+        name: "flask",
         color: "green-text-gradient",
       },
       {
-        name: "css",
+        name: "AWS",
         color: "pink-text-gradient",
       },
     ],
     image: tripguide,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/Sayan2908/ProductReviewer/",
   },
 ];
 
